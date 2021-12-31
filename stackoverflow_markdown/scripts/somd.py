e4d91d90ed42933b2e876e6f3e8e0107b328f6e2
Arnab Sen <arnabsen1729@gmail.com>
2022-01-01 00:48:43 +0530

    add script logic

--- stackoverflow_markdown/scripts/somd.py
+++ stackoverflow_markdown/scripts/somd.py
@@ -1,8 +1,9 @@
 import click
+from stackoverflow_markdown.parser import parse_data
 
 
 @click.command()
-@click.argument('id')
+@click.argument('id', type=int)
 @click.argument('out', type=click.File('w'))
 def cli(id, out):
     """Fetch the stackoverflow question and its answers
@@ -12,4 +13,8 @@ def cli(id, out):
 
     OUT: the output file
     """
-    click.echo(f'Args passed {id} {out}')
+    click.echo(f'[-] Fetching data of question {id}')
+    data = parse_data(id)
+    click.echo(f'[*] Exporting to {out.name}')
+    out.write(data)
+    click.echo('[√] Done!')
